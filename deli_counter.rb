df41b68fd50f134037b9be3634aa2c42a9fdf52f
Danielle Bergbower <dkw5011@gmail.com>
2019-04-28 17:25:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def deli(array)
+  new_array = array.each.with_index.map{|name,index| " #{index + 1}. #{name }"}.join
+  puts "The line is currently:#{new_array}"
+end
+
+def line(array)
+  if array.count == 0
+    puts "The line is currently empty."
+  else
+    deli(array)
+  end
+end
+
+def take_a_number(deli, string)
+  new_array = string.split(/ /)
+  new_deli = deli.concat(new_array)
+  last_name = new_deli.pop
+  number = new_deli.size
+  puts "Welcome, #{last_name}. You are number #{number} in line."
+end
+
+
+  
+  
+  
